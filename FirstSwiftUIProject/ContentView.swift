b714d2f32a7816bb3738946603b800cf32c220d7
fatihfilizol <fatihfilizol@gmail.com>
2022-09-15 18:54:47 +0300

    Initial Commit

---
+++ FirstSwiftUIProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstSwiftUIProject
+//
+//  Created by Fatih Filizol on 15.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstSwiftUIProject/FirstSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIProjectApp.swift
+//  FirstSwiftUIProject
+//
+//  Created by Fatih Filizol on 15.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
